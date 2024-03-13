cd163b3fa62219b90212f4254deb4bfb04969459
yinguohan <13654928852@163.com>
2024-03-13 18:25:45 +0800

    ygh. 小程序第一次提交开发代码。

---
+++ src/components/CztySwiper.vue
@@ -0,0 +1,80 @@
+<!-- 本示例未包含完整css，获取外链css请参考上文，在hello uni-app项目中查看 -->
+<script>
+export default {
+  data() {
+    return {
+      background: ['color1', 'color2', 'color3'],
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 100,
+    };
+  },
+  methods: {
+    changeIndicatorDots(e) {
+      this.indicatorDots = !this.indicatorDots;
+    },
+    changeAutoplay(e) {
+      this.autoplay = !this.autoplay;
+    },
+    intervalChange(e) {
+      this.interval = e.target.value;
+    },
+    durationChange(e) {
+      this.duration = e.target.value;
+    },
+  },
+};
+</script>
+
+<!-- 本示例未包含完整css，获取外链css请参考上文，在hello uni-app项目中查看 -->
+<template>
+  <view>
+    <view class="uni-margin-wrap">
+      <swiper class="swiper" circular :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+        <swiper-item>
+          <img src="../img/20240305152358.jpg" alt="" />
+        </swiper-item>
+        <swiper-item>
+          <img src="../img/20240305152321.jpg" alt="" />
+        </swiper-item>
+        <swiper-item>
+          <!-- <view class="swiper-item uni-bg-blue">C</view> -->
+          <img src="../img/20240305152348.jpg" alt="" />
+        </swiper-item>
+      </swiper>
+    </view>
+  </view>
+</template>
+
+<style>
+.uni-margin-wrap {
+  width: 690rpx;
+  width: 100%;
+}
+.swiper {
+  height: 300rpx;
+}
+.swiper-item {
+  display: block;
+  height: 300rpx;
+  line-height: 300rpx;
+  text-align: center;
+}
+.swiper-list {
+  margin-top: 40rpx;
+  margin-bottom: 0;
+}
+.uni-common-mt {
+  margin-top: 60rpx;
+  position: relative;
+}
+.info {
+  position: absolute;
+  right: 20rpx;
+}
+.uni-padding-wrap {
+  width: 550rpx;
+  padding: 0 100rpx;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
-//
+import { ref, onMounted } from 'vue';
+const noticeText = ref('这是一条滚动通知');
+
+onMounted(() => {
+  // 模拟滚动
+  setInterval(() => {
+    noticeText.value += ' ';
+  }, 1000);
+});
 </script>
 
 <template>
-  <view class="index">index</view>
+  <CztySwiper />
+  <uni-notice-bar :show-icon="true" :scrollable="true" :text="noticeText" mode="closeable" :speed="50" />
 </template>
 
-<style lang="scss">
-//
-</style>
+<style lang="scss"></style>
